1f21d77dc951eb4297dd886f5f73f90966169e88
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-17 22:20:30 +0100

    Created shopping-edit domponent nested in the shopping-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
