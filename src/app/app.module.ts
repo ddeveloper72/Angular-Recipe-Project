fb74e7bd71d840aa83425ba9f73c6e464817073e
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-18 11:49:43 +0100

    Added import for VrowserModule which was raising an error on page not loading,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -21,6 +21,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent
   ],
   imports: [
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
