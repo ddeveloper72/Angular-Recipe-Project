da9d8db52256a3795c33e14b9669f7da92d1e1bc
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-22 18:37:10 +0100

    Created a new property for ingredientsUpdated and implemented EventEmmiter to pass on Ingereient array.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
+  ingredientsUpdated = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -12,6 +14,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsUpdated.emit(this.ingredients.slice());
   }
 
   constructor() { }
