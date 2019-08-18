367a9aeeadddf768c1ba1441d24f6860c36aadfe
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-18 13:35:11 +0100

    Added two new method for returning a new recipe slice for a new or edited recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,32 +6,35 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'Oven-Baked Onion-Cheese Frittata',
       'Quick Italian Recipes at Home',
-    'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg',
-    [
-      new Ingredient('tablespoons olive oil', 2),
-      new Ingredient('small onion; minced ', 1),
-      new Ingredient('cup Parmesan cheese ', 0.5),
-      new Ingredient('eggs', 6),
-    ]),
+      'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg',
+      [
+        new Ingredient('tablespoons olive oil', 2),
+        new Ingredient('small onion; minced ', 1),
+        new Ingredient('cup Parmesan cheese ', 0.5),
+        new Ingredient('eggs', 6)
+      ]
+    ),
     new Recipe(
       'Mediterranean tagliatelle',
       'Quick and easy tagliatelle with olives and sundried tomatoes will transport you to the Mediterranean.',
-    'https://img.taste.com.au/UKtWC_k5/w643-h428-cfill-q90/taste/2016/11/mediterranean-tagliatelle-20864-1.jpeg',
-    [
-      new Ingredient('dried tagliatelle pasta', 250),
-      new Ingredient('pine nuts', 250),
-      new Ingredient('tablespoon olive oil', 1),
-      new Ingredient('garlic clove, chopped', 1),
-      new Ingredient('marinated artichoke pieces, drained', 250)
-    ])
+      'https://img.taste.com.au/UKtWC_k5/w643-h428-cfill-q90/taste/2016/11/mediterranean-tagliatelle-20864-1.jpeg',
+      [
+        new Ingredient('dried tagliatelle pasta', 250),
+        new Ingredient('pine nuts', 250),
+        new Ingredient('tablespoon olive oil', 1),
+        new Ingredient('garlic clove, chopped', 1),
+        new Ingredient('marinated artichoke pieces, drained', 250)
+      ]
+    )
   ];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
@@ -43,6 +46,15 @@ export class RecipeService {
 
   addItemsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
 
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
