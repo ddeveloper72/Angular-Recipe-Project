e88241a687d69c68a44ba39ad27fe6f4a5f1340e
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-31 18:48:44 +0100

    Added the fetchRecipes method which defines the recipes from Firebase as an array.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
@@ -16,5 +17,13 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    this.http
+    .get<Recipe[]>('https://angular-recipe-project-b2193.firebaseio.com/recipes.json')
+    .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+    });
   }
 }
