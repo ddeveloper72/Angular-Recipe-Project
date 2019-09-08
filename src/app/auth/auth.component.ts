44997d5e020eefe7a43ec4792ee74cce3158fd72
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-08 14:51:24 +0100

    used isloading to identify when API reqeust is loading

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
-
 @Component({
     selector: 'app-auth',
-    templateUrl: './auth.component.html'
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
 
     constructor(private authService: AuthService) {}
     onSwitchMode() {
@@ -21,15 +22,18 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
         if (this.isLoginMode) {
             // ...
         } else {
             this.authService.signup(email, password).subscribe(
                 responseData => {
                     console.log(responseData);
+                    this.isLoading = false;
                 },
                 error => {
                     console.log(error);
+                    this.isLoading = false;
                 }
             );
         }
