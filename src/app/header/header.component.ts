8cab582a9e15a9416127b7dfde74618843946667
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-09 22:36:53 +0100

    Setup subscription to authService to check user authentication.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSubscription: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user.subscribe( user => {
+      // this.isAuthenticated = !user ? false : true;
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +31,9 @@ export class HeaderComponent {
 
   onFetchRecipes() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
