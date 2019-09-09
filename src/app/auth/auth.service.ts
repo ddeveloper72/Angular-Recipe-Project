d40f7071a89d98e1e35e4c34c3dd73c92045f401
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-09 13:37:00 +0100

    Added the firebase user login method to the service.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,37 +4,47 @@ import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered: true;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-    constructor(private http: HttpClient) {}
-    signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB1bZ35E939yBfRVynNITYxxVQTqlrZ4EA',
-        {
-            email,
-            password,
-            returnSecureToken: true
-        }
+  constructor(private http: HttpClient) {}
+  signup(email: string, password: string) {
+    return this.http.post < AuthResponseData > (
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB1bZ35E939yBfRVynNITYxxVQTqlrZ4EA', {
+        email,
+        password,
+        returnSecureToken: true
+      }
     ).pipe(catchError(errorResponse => {
-        let errorMessage = 'An unknown error has occurred!';
-        if (!errorResponse.error || !errorResponse.error.error) {
-            return throwError(errorMessage);
-        }
-        switch (errorResponse.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email address has already been taken!';
-        }
+      let errorMessage = 'An unknown error has occurred!';
+      if (!errorResponse.error || !errorResponse.error.error) {
         return throwError(errorMessage);
+      }
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email address has already been taken!';
+      }
+      return throwError(errorMessage);
     }));
-    }
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB1bZ35E939yBfRVynNITYxxVQTqlrZ4EA",
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    );
+  }
 }
