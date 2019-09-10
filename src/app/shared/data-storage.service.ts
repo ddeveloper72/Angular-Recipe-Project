927bf0c23b245a25fc610c0f14ae7eaba4c696ca
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-10 23:17:33 +0100

    Refactored fetchRecipies to uuse the AutInterceptorService to manage combining the user auth token object and the http object.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import {
+  map,
+  tap,
+  take,
+  exhaustMap
+} from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -8,7 +13,10 @@ import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+    ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -23,24 +31,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(take(1),
-    exhaustMap(user => {
-      return this.http
-      .get<Recipe[]>('https://angular-recipe-project-b2193.firebaseio.com/recipes.json',
-      {
-        params: new HttpParams().set('auth', user.token)
-      }
-      );
-    }),
-    map(recipes => {
-        return recipes.map(recipe => {
+    return this.http
+      .get<Recipe[]>(
+        'https://angular-recipe-project-b2193.firebaseio.com/recipes.json'
+        )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
             return {
-                ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-    }),
-    tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-    })
-    );
-  }
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+    }
 }
