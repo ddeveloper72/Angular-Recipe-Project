03a3686c45539c0af37eb3cb9dce097beaea5dd2
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-11 22:25:19 +0100

    Used tap to redirect a user !isAuthenticated to the /auth page.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,26 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
     constructor(
-        private authService: AuthService) {}
+        private authService: AuthService,
+        private router: Router
+        ) {}
     canActivate(
         route: ActivatedRouteSnapshot,
         router: RouterStateSnapshot
         ): boolean | Promise<boolean> | Observable<boolean> {
             return this.authService.user.pipe(map(user => {
                 return !!user;
+            }),
+            tap(isAuthenticated => {
+                if (!isAuthenticated) {
+                    this.router.navigate(['/auth']);
+                }
             }));
         }
 }
