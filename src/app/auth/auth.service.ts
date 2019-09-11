13e5273a1d0f925e325f308c9359e1d9cd7155d0
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-11 13:43:27 +0100

    Added an autoLogin method in the authService that strores the user auth data in local storage.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -44,6 +44,27 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+      );
+    if (loadedUser.token) {
+        this.user.next(loadedUser);
+      }
+  }
+
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
@@ -89,6 +110,7 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
