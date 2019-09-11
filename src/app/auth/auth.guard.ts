2b007a6fe1a168846f38fbeb17d29ffe5af0237c
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-11 22:52:02 +0100

    Switched to using urlTree to prevent multiple redirect race conditions.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,26 +1,40 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { 
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router,
+    UrlTree
+    } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    constructor(
-        private authService: AuthService,
-        private router: Router
-        ) {}
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        router: RouterStateSnapshot
-        ): boolean | Promise<boolean> | Observable<boolean> {
-            return this.authService.user.pipe(map(user => {
-                return !!user;
-            }),
-            tap(isAuthenticated => {
-                if (!isAuthenticated) {
-                    this.router.navigate(['/auth']);
-                }
-            }));
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Promise<boolean | UrlTree>
+    | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      map(user => {
+        const isAuthenticated = !!user;
+        if (isAuthenticated) {
+          return true;
         }
+        return this.router.createUrlTree(['/auth']);
+      })
+
+      // tap(isAuthenticated => {
+      //     if (!isAuthenticated) {
+      //         this.router.navigate(['/auth']);
+      //     }
+      // }
+    );
+  }
 }
