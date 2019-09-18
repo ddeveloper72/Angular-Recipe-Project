fb63b01a84d6636ef41e1dc02658ea972f71d43c
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-18 13:39:41 +0100

    Added import for RouterModule

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -14,6 +15,9 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeItemComponent,
         RecipeStartComponent,
         RecipeEditComponent
+    ],
+    imports: [
+        RouterModule
     ],
     exports: [
         RecipesComponent,
