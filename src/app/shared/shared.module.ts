3d3204021cc9d6aeef80c92d12086be60cdb9d39
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-23 22:23:12 +0100

    Created a shared module, cleaning up the app module.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { AlertComponent } from './alert/alert.component';
+import { PlaceholderDirective } from './placeholder.directive';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@NgModule({
+    declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective
+    ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        BsDropdownModule.forRoot()
+    ],
+    exports: [
+        AlertComponent,
+        LoadingSpinnerComponent,
+        PlaceholderDirective,
+        CommonModule,
+        FontAwesomeModule,
+        BsDropdownModule
+    ],
+  entryComponents: [AlertComponent]
+})
+
+export class SharedModule {}
