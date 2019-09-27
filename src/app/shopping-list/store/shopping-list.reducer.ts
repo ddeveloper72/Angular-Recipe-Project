314662824e9dcd40f0efb1905f2a8e5236cd476d
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-27 13:42:21 +0100

    Corrected error in ingredients array for the initial state.

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -2,7 +2,7 @@ import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list actions';
 
 const initialState = {
-    ingredients: Ingredient[] = [
+    ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ]
