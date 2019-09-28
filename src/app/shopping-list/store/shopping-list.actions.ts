e828b68e2419db4ff52c6a209965edf09a90a564
ddeveloper72 <duncan.niamh@gmail.com>
2019-09-28 22:18:08 +0100

    Removed stray import.

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,6 +1,5 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
-import { type } from 'os';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
