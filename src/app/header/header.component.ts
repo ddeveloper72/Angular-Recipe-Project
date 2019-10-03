108d65b31f5ab07522a97a1f23a950143fd00408
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-03 13:56:10 +0100

    Imported app.reducer to retrieve the auth user.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import * as fromApp from '../store/app.reducer';
+
 
 @Component({
   selector: 'app-header',
@@ -14,11 +18,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.userSubscription = this.authService.user.subscribe(user => {
+    this.userSubscription = this.store
+    .select('auth')
+    .pipe(map(authState => authState.user))
+    .subscribe(user => {
       // this.isAuthenticated = !user ? false : true;
       this.isAuthenticated = !!user;
       console.log(!user);
