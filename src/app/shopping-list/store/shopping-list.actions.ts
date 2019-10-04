9ac06d0c1c23f5c624134adc0a938936a6ec420f
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-04 13:31:08 +0100

    Added component names to the start and stop edit identifiers as well.

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,8 +5,8 @@ export const ADD_INGREDIENT = '[Shopping List] Add Ingredient';
 export const ADD_INGREDIENTS = '[ADD Shopping List] Add Ingredients';
 export const UPDATE_INGREDIENT = '[Shopping List] Update Ingredient';
 export const DELETE_INGREDIENT = '[Shopping List] Delete Ingredient';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const START_EDIT = '[Shopping List] Start Edit';
+export const STOP_EDIT = '[Shopping List] Stop Edit';
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
     constructor(public payload: Ingredient) {}
