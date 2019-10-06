41710f905d3e0595254cc7b52530f0faf1daf4c3
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-06 20:59:30 +0100

    Created handleAuthentication & handleError constants to remove duplication of code in authSighup & authLogin.

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -18,8 +18,71 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
+const handleAuthentication = (
+  expiresIn: number,
+  email: string,
+  userId: string,
+  token: string
+  ) => {
+    const expirationDate = new Date(
+      new Date().getTime() + +expiresIn * 1000); // convert time from milliseconds to seconds
+    return new AuthActions.AuthenticateSuccess({
+      email,
+      userId,
+      token,
+      expirationDate
+    });
+};
+
+const handleError = (errorResponse: any) => {
+  let errorMessage = 'An unknown error has occurred!';
+  if (!errorResponse.error || !errorResponse.error.error) {
+    return of(new AuthActions.AuthenticateFail(errorMessage));
+  }
+  switch (errorResponse.error.error.message) {
+    case 'EMAIL_EXISTS':
+      errorMessage = 'This email address has already been taken!';
+      break;
+    case 'EMAIL_NOT_FOUND':
+      errorMessage = 'Please check your email address';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMessage = 'Please check your password!';
+  }
+  return of(new AuthActions.AuthenticateFail(errorMessage));
+};
+
 @Injectable()
 export class AuthEffects {
+  @Effect()
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.SIGNUP_START),
+    switchMap((signupAction: AuthActions.SignupStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+            environment.firebaseAPIKey,
+          {
+            email: signupAction.payload.email,
+            password: signupAction.payload.password,
+            returnSecureToken: true
+          }
+        )
+        .pipe(map(responseData => {
+          return handleAuthentication(
+            +responseData.expiresIn,
+            responseData.email,
+            responseData.localId,
+            responseData.idToken
+            );
+          }),
+          catchError(errorResponse => {
+            return handleError(errorResponse);
+          })
+        );
+    })
+  );
+
   @Effect()
   authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START),
@@ -35,31 +98,14 @@ export class AuthEffects {
           }
         )
         .pipe(map(responseData => {
-            const expirationDate = new Date(
-              new Date().getTime() + +responseData.expiresIn * 1000
-              ); // convert time from milliseconds to seconds
-            return new AuthActions.Login({
-              email: responseData.email,
-              userId: responseData.localId,
-              token: responseData.idToken,
-              expirationDate
-            });
-          }), catchError(errorResponse => {
-            let errorMessage = 'An unknown error has occurred!';
-            if (!errorResponse.error || !errorResponse.error.error) {
-              return of(new AuthActions.LoginFail(errorMessage));
-            }
-            switch (errorResponse.error.error.message) {
-              case 'EMAIL_EXISTS':
-                errorMessage = 'This email address has already been taken!';
-                break;
-              case 'EMAIL_NOT_FOUND':
-                errorMessage = 'Please check your email address';
-                break;
-              case 'INVALID_PASSWORD':
-                errorMessage = 'Please check your password!';
-            }
-            return of(new AuthActions.LoginFail(errorMessage));
+            return handleAuthentication(
+              +responseData.expiresIn,
+              responseData.email,
+              responseData.localId,
+              responseData.idToken);
+          }),
+          catchError(errorResponse => {
+            return handleError(errorResponse);
           })
         );
     })
@@ -67,7 +113,7 @@ export class AuthEffects {
 
   @Effect({ dispatch: false })
   authSuccess = this.actions$.pipe(
-    ofType(AuthActions.LOGIN),
+    ofType(AuthActions.AUTHENTICATE_SUCCESS),
     tap(() => {
       this.router.navigate(['/']);
     })
