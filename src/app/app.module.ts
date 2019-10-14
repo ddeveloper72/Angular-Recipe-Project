2bd86b895636a14fd417e607b561b435311a08e9
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-14 21:47:03 +0100

    Added imports for @ngrx/router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { environment } from '../environments/environment';
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule
   ],
   providers: [
