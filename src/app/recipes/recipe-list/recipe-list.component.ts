32b2a02212c39e5ec82c79e49a791cccce0bf249
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-15 13:59:12 +0100

    Removed the recipeService and replaced with the recipe state from ngrx.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/Operators';
+import { Store } from '@ngrx/store';
 
+import { Recipe } from '../recipe.model';
+import * as fromApp from '../../store/app.reducer';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -13,19 +15,20 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService,
+  constructor(
               private router: Router,
-              private route: ActivatedRoute ) {
+              private route: ActivatedRoute,
+              private store: Store<fromApp.AppState> ) {
   }
 
   ngOnInit() {
-    this.subscription = this.recipeService.recipesChanged
+    this.subscription = this.store.select('recipes')
+    .pipe(map(recipesState => recipesState.recipes))
     .subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
-    this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
