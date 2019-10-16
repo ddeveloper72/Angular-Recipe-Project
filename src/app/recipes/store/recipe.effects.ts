5679b04b5c188dc6116093b699396a2250d4b321
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-16 13:44:07 +0100

    Added loginc for getting the recipe to recipe.effects.

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,3 +1,20 @@
-export interface recipeEffects {
-
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import * as RecipesActions from './recipe.actions';
+import { switchMap } from 'rxjs/operators';
+import { Recipe } from '../recipe.model';
+export class RecipeEffects {
+    @Effect()
+    fetchRecipes = this.actions$.pipe(
+        ofType(RecipesActions.FETCH_RECIPES),
+        switchMap(() => {
+            return this.http.get<Recipe[]>(
+              'https://angular-recipe-project-b2193.firebaseio.com/recipes.json'
+            );
+        })
+        );
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient
+        ) {}
 }
