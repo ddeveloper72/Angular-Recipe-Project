3a0ff518e4f415015b151a9ed6ec0f6d72213539
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-16 13:54:35 +0100

    Added return for get recipes from API

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,20 +1,30 @@
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, map } from 'rxjs/operators';
 import * as RecipesActions from './recipe.actions';
-import { switchMap } from 'rxjs/operators';
 import { Recipe } from '../recipe.model';
+@Injectable()
 export class RecipeEffects {
-    @Effect()
-    fetchRecipes = this.actions$.pipe(
-        ofType(RecipesActions.FETCH_RECIPES),
-        switchMap(() => {
-            return this.http.get<Recipe[]>(
-              'https://angular-recipe-project-b2193.firebaseio.com/recipes.json'
-            );
-        })
-        );
-    constructor(
-        private actions$: Actions,
-        private http: HttpClient
-        ) {}
-}
+         @Effect()
+         fetchRecipes = this.actions$.pipe(
+           ofType(RecipesActions.FETCH_RECIPES),
+           switchMap(() => {
+             return this.http.get<Recipe[]>(
+               'https://angular-recipe-project-b2193.firebaseio.com/recipes.json'
+             );
+           }),
+           map(recipes => {
+             return recipes.map(recipe => {
+               return {
+                 ...recipe,
+                 ingredients: recipe.ingredients ? recipe.ingredients : []
+               };
+             });
+           }),
+           map(recipes => {
+               return new RecipesActions.SetRecipes(recipes);
+           })
+         );
+         constructor(private actions$: Actions, private http: HttpClient) {}
+       }
