e3c608760303b1420eb198280d3d80373123f2a7
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-18 21:58:48 +0100

    Added the UPDATE_RECIPE case.

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -15,16 +15,29 @@ export function recipeReducer(
     ) {
     switch (action.type) {
       case RecipeActions.SET_RECIPES:
-          return {
-              ...state,
-              recipes: [...action.payload]
-          };
-          case RecipeActions.ADD_RECIPE:
-          return {
-              ...state,
-              recipes: [...state.recipes, action.payload]
-          };
-          default:
-              return state;
+        return {
+          ...state,
+          recipes: [...action.payload]
+        };
+      case RecipeActions.ADD_RECIPE:
+        return {
+          ...state,
+          recipes: [...state.recipes, action.payload]
+        };
+      case RecipeActions.UPDATE_RECIPE:
+        const updatedRecipe = {
+          ...state.recipes[action.payload.index],
+          ...action.payload.newRecipe
+        };
+
+        const updatedRecipes = [...state.recipes];
+        updatedRecipes[action.payload.index] = updatedRecipe;
+
+        return {
+          ...state,
+          recipe: updatedRecipes
+        };
+      default:
+        return state;
     }
 }
