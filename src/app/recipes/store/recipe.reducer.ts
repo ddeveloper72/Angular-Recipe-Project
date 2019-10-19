9c4c54bc6577797dd6edc556b8bd8d914148f949
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-19 17:39:35 +0100

    Corrected RecipeActions which was used mixed with ReciopesActions

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,5 +1,5 @@
 import { Recipe } from '../recipe.model';
-import * as RecipeActions from './recipe.actions';
+import * as RecipesActions from './recipe.actions';
 
 export interface State {
     recipes: Recipe[];
@@ -10,41 +10,41 @@ const initialState: State = {
 };
 
 export function recipeReducer(
-    state = initialState,
-    action: RecipeActions.RecipesActions
-    ) {
-    switch (action.type) {
-      case RecipeActions.SET_RECIPES:
-        return {
-          ...state,
-          recipes: [...action.payload]
-        };
-      case RecipeActions.ADD_RECIPE:
-        return {
-          ...state,
-          recipes: [...state.recipes, action.payload]
-        };
-      case RecipeActions.UPDATE_RECIPE:
-        const updatedRecipe = {
-          ...state.recipes[action.payload.index],
-          ...action.payload.newRecipe
-        };
+  state = initialState,
+  action: RecipesActions.RecipesActions
+       ) {
+         switch (action.type) {
+           case RecipesActions.SET_RECIPES:
+             return {
+               ...state,
+               recipes: [...action.payload]
+             };
+           case RecipesActions.ADD_RECIPE:
+             return {
+               ...state,
+               recipes: [...state.recipes, action.payload]
+             };
+           case RecipesActions.UPDATE_RECIPE:
+             const updatedRecipe = {
+               ...state.recipes[action.payload.index],
+               ...action.payload.newRecipe
+             };
 
-        const updatedRecipes = [...state.recipes];
-        updatedRecipes[action.payload.index] = updatedRecipe;
+             const updatedRecipes = [...state.recipes];
+             updatedRecipes[action.payload.index] = updatedRecipe;
 
-        return {
-          ...state,
-          recipe: updatedRecipes
-        };
-        case RecipeActions.DELETE_RECIPE:
-        return {
-            ...state,
-            recipes: state.recipes.filter((recipe, index) => {
-                return index !== action.payload;
-            })
-        };
-      default:
-        return state;
-    }
-}
+             return {
+               ...state,
+               recipes: updatedRecipes
+             };
+           case RecipesActions.DELETE_RECIPE:
+             return {
+               ...state,
+               recipes: state.recipes.filter((recipe, index: number) => {
+                 return index !== action.payload;
+               })
+             };
+           default:
+             return state;
+         }
+       }
