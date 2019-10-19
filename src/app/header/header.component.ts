2be62e671a7be9a6b6e0bfeb37a15c01d16ecefc
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-19 21:26:32 +0100

    Removed the data storage service and implemented the ngrx store reciupes action.  Corrected RecipesAction as well.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,9 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { DataStorageService } from '../shared/data-storage.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthAction from '../auth/store/auth.actions';
-import * as RecipeActions from '../recipes/store/recipe.actions';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 
 @Component({
@@ -19,7 +18,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSubscription: Subscription;
 
   constructor(
-    private dataStorageService: DataStorageService,
     private store: Store<fromApp.AppState>
   ) {}
 
@@ -36,12 +34,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+    // this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipesActions.StoreRecipes());
   }
 
   onFetchRecipes() {
     // this.dataStorageService.fetchRecipes().subscribe();
-    this.store.dispatch(new RecipeActions.FetchRecipes());
+    this.store.dispatch(new RecipesActions.FetchRecipes());
   }
 
   onLogout() {
