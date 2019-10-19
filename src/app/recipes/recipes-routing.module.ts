6d6c80e59e2b9007a76b5b2ba87ff273e54c598a
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-19 17:41:22 +0100

    Removed import for NgModel which was never used.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes.component';
 import { AuthGuard } from '../auth/auth.guard';
@@ -6,7 +6,6 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesResolverService } from './recipes-resolver.service';
-import { NgModule } from '@angular/core';
 
 const routes: Routes = [
     { path: '', component: RecipesComponent,
