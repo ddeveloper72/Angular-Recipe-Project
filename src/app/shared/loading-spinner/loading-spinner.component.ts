c901de3b1482e43ae41dc4b63d7e8c69aabe6841
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-24 22:54:38 +0100

    Corrected uppercase char in name of css file

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-loading-spinner',
     template: '<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
-    styleUrls: ['./loading-spinner.Component.css']
+    styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {}
