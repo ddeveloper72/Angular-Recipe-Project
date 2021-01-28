d3eaa71a2d7f9b0449cbba56e3da8ab4a59a5e6a
ddeveloper72 <duncan.niamh@gmail.com>
2021-01-28 17:59:46 +0000

    corrected typo in comment

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
